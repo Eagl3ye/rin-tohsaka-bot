3474b1e3bf5eb6c1d0f5ae8ec12b57c700e1b356
Eagl3ye <vincejaphethloable@gmail.com>
2020-08-29 13:58:24 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -90,40 +90,34 @@ async def baog(ctx):
 
 @client.command()
 async def ex(ctx):
-	await ctx.send('Wished by <@487935377219256343>')
-	embed = discord.Embed(
-		description='**Monkey D. Luffy** \n\nOne Piece\n**565**<:kakera:748810456671453296>',
-		colour=discord.Colour.green()
-		)
-	embed.set_image(url='https://i.imgur.com/9UVKIr1.png')
-	msg = await ctx.send(embed=embed)
-	await msg.add_reaction('\U0001F496')
-	
-	
-	count = 0
-	useruser = 0
-	def check(reaction, user):
-		if count < 0:
+	if ctx.message.author.id = 336068309789310979 or ctx.message.author.id = 487935377219256343:
+		await ctx.send('Wished by <@487935377219256343>')
+		embed = discord.Embed(
+			description='**Monkey D. Luffy** \n\nOne Piece\n**565**<:kakera:748810456671453296>',
+			colour=discord.Colour.green()
+			)
+		embed.set_image(url='https://i.imgur.com/9UVKIr1.png')
+		msg = await ctx.send(embed=embed)
+		await msg.add_reaction('\U0001F496')
+		
+		new_embed = discord.Embed(
+			description='**Monkey D. Luffy** \n\nOne Piece\n**565**<:kakera:748810456671453296>',
+			colour=discord.Colour.red()
+			)
+		new_embed.set_image(url='https://i.imgur.com/9UVKIr1.png')
+		new_embed.set_footer(text='Belongs to Reinn_sama', icon_url='https://cdn.discordapp.com/avatars/487935377219256343/2656f554ae5e6ff7d703512f29414984.png')
+
+		def check(reaction, user):
 			print(str(user.id))
 			id_list = [336068309789310979,487935377219256343]
-			useruser = user
-			count += 1
 			return (reaction.message.id == msg.id and user.id in id_list) #487935377219256343
-
-	new_embed = discord.Embed(
-		description='**Monkey D. Luffy** \n\nOne Piece\n**565**<:kakera:748810456671453296>',
-		colour=discord.Colour.red()
-		)
-	new_embed.set_image(url='https://i.imgur.com/9UVKIr1.png')
-	new_embed.set_footer(text='Belongs to Reinn_sama', icon_url=useruser.avatar_url)
-
-	try:
-		reaction, user = await client.wait_for("reaction_add", check=check, timeout=25)
-	except asyncio.TimeoutError:
-		print('Timeout')
-	else:
-		print(str(useruser))
-		await msg.edit(embed=new_embed)
-		await ctx.send('Welcome to the Kingdom of ♕ **Reinn_sama, Monkey D. Luffy**! :european_castle:')
+		try:
+			reaction, user = await client.wait_for("reaction_add", check=check, timeout=15)
+		except asyncio.TimeoutError:
+			print('Timeout')
+		else:
+			print('Rheana reacted')
+			await msg.edit(embed=new_embed)
+			await ctx.send('Welcome to the Kingdom of ♕ **Reinn_sama, Monkey D. Luffy**! :european_castle:')
 
 client.run(os.environ['TOKEN'])
