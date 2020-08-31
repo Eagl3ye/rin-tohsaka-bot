64c00921ebdaa877809c4f2eddebd5ba66ba5d6c
Eagl3ye <vincejaphethloable@gmail.com>
2020-08-31 09:42:36 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ async def mudae_event(ctx, mode=None, number=1):
 		if number < 1:
 			number = 1
 		for _ in range(number):
-			embed.add_field(name=':outbox_tray:| '+mudae_events_list[mudae_randomize()][0], value=+mudae_events_list[mudae_randomize()][0], inline=False)
+			embed.add_field(name=':outbox_tray:| '+mudae_events_list[mudae_randomize()][0], value=mudae_events_list[mudae_randomize()][0], inline=False)
 		#letters = ['a','b','c','d','e','f','g','h','i','j','k']
 		embed.set_footer(text='New events will be added later... stay tuned!')
 		#await ctx.send(content=":heart_exclamation: Attention to all <@&742333248125927425> :heart_exclamation:")
